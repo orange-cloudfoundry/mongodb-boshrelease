1babc172db1f5120567429f2d600a9495ed42ddd
jraverdy-orange <jeremy.raverdy@gmail.com>
2017-09-14 23:07:50 +0000

    First Upload

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# mongodb-bosh-release
+# <p style="text-align:center">Mongodb Bosh Release</p>
+##########
+This is a mongodb bosh release with from source compilation.
+It include rocksdb engine and tls/ssl encryption 
+
+Detail are coming soon...
+
+* 2017-09-15
+Still work in progress- Just a code backup
+**Should not be used as it is**
---
+++ jobs/mongodb-server/templates/bin/mongo.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e # exit immediately if a simple command exits with a non-zero status.
+set -u # report the usage of uninitialized variables.
+
+source `dirname $(readlink --canonicalize-existing $0)`/setenv
+
+export PARAM="$@"
+
+pushd /var/vcap/packages/mongodb/bin >/dev/null
+su -m vcap -c 'HOME=/home/vcap;'${MONGODB_BIN}'/mongo $PARAM'
+popd >/dev/null
+exit 0
