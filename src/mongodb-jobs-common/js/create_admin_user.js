03c5e2df699aa6deb9a03bb7e54f5ec7fc3abe76
Jérémy Raverdy <jeremy.raverdy@orange.com>
2018-05-15 13:45:04 +0200

    Using a mongo stored function to retrieve deployment variables instead of using an  erb

--- src/mongodb-jobs-common/js/create_admin_user.js
+++ src/mongodb-jobs-common/js/create_admin_user.js
@@ -1,33 +1,46 @@
-<%
-
-require "json"
-
-def esc(str)
-    str.to_json[1..-2]
-end
-
--%>
 use admin;
 
-if (db.system.users.find({ user: "<%= esc(p('admin_username')) %>" }).count() == 0) {
+// retrieve variables from conf file
+// job_dir must be passed when calling mongo shell using the --eval and --shell option
+
+db.system.js.save({_id: "getDeploymentVar",
+                   value : function(key)
+                   {
+                      var file=cat(job_dir+"/bin/mdb-variables.sh");
+                      var objectId = file.split('\n');
+                      value=null;
+                      for (var i =0; i<objectId.length-1; i++){
+                        var keyval = objectId[i].split("=");
+                        if (keyval[0] == key)
+                        {
+                        value=keyval[1];
+                        }
+                      }
+                      return value;
+                    }
+});
+
+db.loadServerScripts("getDeploymentVar");
+
+if (db.system.users.find({ user: getDeploymentVar("property_admin_username") }).count() == 0) {
     db.createUser({
-        user: "<%= esc(p('admin_username')) %>",
-        pwd: "<%= esc(p('admin_password')) %>",
+        user: getDeploymentVar("property_admin_username"),
+        pwd: getDeploymentVar("property_admin_password"),
         roles: [
             { role: "userAdminAnyDatabase", db: "admin" }
         ]
     });
 } else {
-    db.changeUserPassword("<%= esc(p('admin_username')) %>", "<%= esc(p('admin_password')) %>");
+    db.changeUserPassword(getDeploymentVar("property_admin_username"), getDeploymentVar("property_admin_password"));
 }
-if (db.system.users.find({ user: "<%= esc(p('root_username')) %>" }).count() == 0) {
+if (db.system.users.find({ user: getDeploymentVar("property_root_username") }).count() == 0) {
     db.createUser({
-        user: "<%= esc(p('root_username')) %>",
-        pwd: "<%= esc(p('root_password')) %>",
+        user: getDeploymentVar("property_root_username"),
+        pwd: getDeploymentVar("property_root_password"),
         roles: [
             { role: "root", db: "admin" }
         ]
     });
 } else {
-    db.changeUserPassword("<%= esc(p('root_username')) %>", "<%= esc(p('root_password')) %>");
+    db.changeUserPassword(getDeploymentVar("property_root_username"), getDeploymentVar("property_root_password"));
 }
