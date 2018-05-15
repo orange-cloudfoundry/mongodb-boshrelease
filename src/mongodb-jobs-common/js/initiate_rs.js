6742e345d036bba79d8a3b325bc9926674ac1153
Jérémy Raverdy <jeremy.raverdy@orange.com>
2018-05-15 15:12:44 +0200

    Cannot store function while replicaset is not initialized. Use volatile function instead

--- src/mongodb-jobs-common/js/initiate_rs.js
+++ src/mongodb-jobs-common/js/initiate_rs.js
@@ -3,24 +3,19 @@ use admin;
 // retrieve variables from conf file
 // job_dir must be passed when calling mongo shell using the --eval and --shell option
 
-db.system.js.save({_id: "getDeploymentVar",
-                   value : function(key)
-                   {
-                      var file=cat(job_dir+"/bin/mdb-variables.sh");
-                      var objectId = file.split('\n');
-                      value=null;
-                      for (var i =0; i<objectId.length-1; i++){
-                        var keyval = objectId[i].split("=");
-                        if (keyval[0] == key)
-                        {
-                        value=keyval[1];
-                        }
-                      }
-                      return value;
-                    }
-});
-
-db.loadServerScripts("getDeploymentVar");
+function getDeploymentVar(key){
+  var file=cat(job_dir+"/bin/mdb-variables.sh");
+  var objectId = file.split('\n');
+  value=null;
+  for (var i =0; i<objectId.length-1; i++){
+    var keyval = objectId[i].split("=");
+    if (keyval[0] == key)
+    {
+    value=keyval[1];
+    }
+  }
+  return value;
+}
 
 rs.initiate({
     _id : getDeploymentVar("property_replica_set_name"),
