d049ff8db4de3ca65b6f5b189b60b32c75ec37a6
Jérémy Raverdy <jeremy.raverdy@orange.com>
2018-05-16 11:32:09 +0200

    Making connect script common for the different jobs

--- src/mongodb-jobs-common/bin/mongoadmin.sh
+++ src/mongodb-jobs-common/bin/mongoadmin.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-source `dirname $(readlink -f $0)`/setenv
+export JOBS_COMMON_DIR=/var/vcap/packages/mongodb-jobs-common
+export JOB_NAME=$1
+
+source ${JOBS_COMMON_DIR}/bin/setenv ${JOB_NAME}
+
+source ${JOB_DIR}/bin/mdb-variables.sh
 
 if [ ${property_require_ssl} == 0 ]
 then
@@ -8,8 +13,6 @@ then
 else	
 	export MONGO_CMD="${JOBS_COMMON_DIR}/bin/mongo-ssl.sh"
 fi
-
-mongo_DbTypeport = ${property_mongod_listen_port}
 
 export DB=$1
 shift
@@ -19,10 +22,14 @@ pushd /var/vcap/packages/mongodb/bin >/dev/null
 if [ "${property_node_role}" == "sa" ]
 then
 	${MONGO_CMD} mongodb://${deployment_rs_config}/${DB} \
- 	-u ${property_root_username} %> -p ${property_root_password} %> --authenticationDatabase admin ${PARAM}
-else 	
-	${MONGO_CMD} mongodb://${deployment_rs_config}/${DB}?replicaSet=<%= p("replication.replica_set_name") %> \
- 	-u ${property_root_username} %> -p ${property_root_password} %> --authenticationDatabase admin ${PARAM}
+		-u ${property_root_username} \
+		-p ${property_root_password} \
+		--authenticationDatabase admin ${PARAM}
+	else
+	${MONGO_CMD} mongodb://${deployment_rs_config}/${DB}?replicaSet=${property_replica_set_name} \
+		-u ${property_root_username} \
+		-p ${property_root_password} \
+		--authenticationDatabase admin ${PARAM}
 fi
 
 popd >/dev/null
