669a2ebde5bd205827ff040d6f1034489ad91d19
Jérémy Raverdy <jeremy.raverdy@orange.com>
2018-05-29 18:02:41 +0200

    report ssl modifications done in upper commits

--- jobs/mongod/templates/bin/mongo.sh
+++ jobs/mongod/templates/bin/mongo.sh
@@ -1,13 +1,13 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -e # exit immediately if a simple command exits with a non-zero status.
 set -u # report the usage of uninitialized variables.
 
 source `dirname $(readlink -f $0)`/setenv
 
-export PARAM="$@"
+PARAM="$@"
 
 pushd /var/vcap/packages/mongodb/bin >/dev/null
-exec chpst -u vcap:vcap env HOME=/home/vcap ${MONGODB_BIN}/mongo $PARAM
+exec chpst -u vcap:vcap env HOME=/home/vcap ${MONGODB_BIN}/mongo ${PARAM}
 popd >/dev/null
 exit 0
