f3585561c2b1eee5b3597c648ce95632a2848aae
Jérémy RAVERDY <jeremy.raverdy@orange.com>
2019-01-31 18:43:18 +0100

    connecting cf

--- ci/scripts/create-service-broker.sh
+++ ci/scripts/create-service-broker.sh
@@ -2,4 +2,10 @@
 
 set -ex
 
-cf --version
+if [ "${SKIP_SSL}" == "true" ]
+then
+    opts="${opts} --skip-ssl-validation"
+fi
+
+cf api ${CF_API_URL} ${opts}
+cf login -u admin -p ${CF_ADMIN_PASSWORD}
