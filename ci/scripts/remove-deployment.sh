f54a5bb1ec102cef0201eaa558d749e447bee0e7
Jérémy RAVERDY <jeremy.raverdy@orange.com>
2019-01-31 10:59:37 +0100

    Adding job to delete deployment and release after tests

--- ci/scripts/remove-deployment.sh
+++ ci/scripts/remove-deployment.sh
@@ -6,13 +6,6 @@ export ROOT_FOLDER=${PWD}
 
 export BOSH_CONFIG=$PWD/bosh-director-config/bosh_config.yml
 
-if [ "${STEMCELL_TYPE}" == "centos" ]
-then
-    # If we are on a centos deployment, deloyment name and release name will be suffixed
-    DEPLOYMENT_NAME="${DEPLOYMENT_NAME}-centos"
-    RELEASE_NAME="${RELEASE_NAME}-centos"
-fi
-
 bosh -e ${ALIAS} -n -d ${DEPLOYMENT_NAME} \
 		delete-deployment
 
@@ -30,9 +23,3 @@ bosh -e ${ALIAS} disks --orphaned --column "Disk CID" --column "Deployment" \
 											}
 										}' \
 	| xargs -i -t bosh -e ${ALIAS} -n -d ${DEPLOYMENT_NAME} delete-disk {}
-
-mkdir -p removed
-
-pushd removed || exit 666
-touch keyval.properties
-popd
