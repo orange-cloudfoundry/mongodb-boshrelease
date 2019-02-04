252995c1e11495a3ca492c78226205e7d1262f1e
Jérémy RAVERDY <jeremy.raverdy@orange.com>
2019-02-04 17:40:51 +0100

    Removing cf components

---
+++ ci/scripts/remove-cf-components.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash 
+
+set -ex
+
+if [ "${SKIP_SSL}" == "true" ]
+then
+    opts="${opts} --skip-ssl-validation"
+fi
+
+cf api ${CF_API_URL} ${opts}
+cf login -u admin -p ${CF_ADMIN_PASSWORD}
+
+#cf unbind-service mongodb-example-app mongodb-instance
+
+#cf delete-service -f mongodb-instance
+
+cf delete -f mongodb-example-app
+
+cf delete-service-broker -f mongodb
+
