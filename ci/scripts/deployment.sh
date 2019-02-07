a828488f4fce8c470180aa389d92c886d50d16a5
Jérémy RAVERDY <jeremy.raverdy@orange.com>
2019-02-07 11:34:43 +0100

    split release creation and deployment in two sifferent jobs

--- ci/scripts/deployment.sh
+++ ci/scripts/deployment.sh
@@ -1,70 +1,9 @@
 #!/usr/bin/env bash 
 
-set -e
+set -ex
 
 export ROOT_FOLDER=${PWD}
 
-create_fake_files()
-{
-    # Creating fake files for already deployed releases
-
-    if [ ! -d dev_releases/${RELEASE_NAME} ]
-    then
-        mkdir -p dev_releases/${RELEASE_NAME}
-    fi
-
-    bosh -e ${ALIAS} releases \
-            | cat \
-            | awk -v rn=${RELEASE_NAME} '{
-                                            if ($1==rn)
-                                                {
-                                                    gsub("\*","");
-                                                    printf "%s",$2;
-                                                    gsub("\+","");
-                                                    printf " %s\n",$3
-                                                }
-                                        }' \
-            | while read version commit_hash                            
-              do
-     
-                if [ ! -f dev_releases/${RELEASE_NAME}/index.yml ]
-                then
-                    echo "builds:" > dev_releases/${RELEASE_NAME}/index.yml
-                fi
-                if [ ! -f dev_releases/${RELEASE_NAME}/${RELEASE_NAME}-${version}.yml ]
-                then
-		            cat > dev_releases/${RELEASE_NAME}/${RELEASE_NAME}-${version}.yml <<-EOF
-							name: ${DEPLOYMENT_NAME}
-							version: ${version}
-							commit_hash: ${commit_hash}
-							uncommitted_changes: false
-							EOF
-		            cat >> dev_releases/${RELEASE_NAME}/index.yml <<-EOF
-							  $(cat /proc/sys/kernel/random/uuid):
-							    version: ${version}
-							EOF
-		        fi
-    		done    
-}
-
-export BOSH_CONFIG=$PWD/bosh-director-config/bosh_config.yml
-
-pushd mongodb-bosh-release-patched || exit 666
-
-create_fake_files
-
-# renaming final_name in final.yml
-
-sed -i -e "s/\(^final_name: \).*$/\1 ${RELEASE_NAME}/" config/final.yml
-
-# avoid checking jobs fingerprints
-for i in $(find .final_builds -type d ! -path '*/packages' \
-                           ! -path '*/packages/golang*' \
-                           ! -path '.final_builds' \
-                           -print )
-do
-        [ -d $i ] && rm -rf $i
-done
 deployment_var_init="   -v deployment_name=${DEPLOYMENT_NAME} \
                         -v release_name=${RELEASE_NAME} \
                         -v deployments-network=${DEPLOYMENT_NETWORK} \
@@ -78,7 +17,7 @@ deployment_var_init="   -v deployment_name=${DEPLOYMENT_NAME} \
 deployment_ops_files_cmd=""
 for i in ${OPSFILES}
 do
-  deployment_ops_files_cmd="${deployment_ops_files_cmd} -o ${ROOT_FOLDER}/mongodb-bosh-release-patched/operations/$i"
+  deployment_ops_files_cmd="${deployment_ops_files_cmd} -o ${ROOT_FOLDER}/mongodb-bosh-release/operations/$i"
 done  
 
 # if using broker opsfiles, setting the appropriate variables
@@ -120,7 +59,7 @@ fi
 if [ "${ENGINE}" == "rocksdb" ]
 then
     deployment_ops_files_cmd="${deployment_ops_files_cmd} \
-                    -o ${ROOT_FOLDER}/mongodb-bosh-release-patched/operations/use-rocksdb.yml"
+                    -o ${ROOT_FOLDER}/mongodb-bosh-release/operations/use-rocksdb.yml"
 fi
 
 if [ "${REQUIRE_SSL}" == "true" ]
@@ -129,12 +68,8 @@ then
                     -v ca_name=${CA_NAME}"
 fi    
 
-bosh -e ${ALIAS} cr --force
-
-bosh -e ${ALIAS} ur 
-
 bosh -e ${ALIAS} deploy -n -d ${DEPLOYMENT_NAME} \
-        ${ROOT_FOLDER}/mongodb-bosh-release-patched/manifests/${MANIFEST} \
+        ${ROOT_FOLDER}/mongodb-bosh-release/manifests/${MANIFEST} \
         ${deployment_ops_files_cmd} \
         ${deployment_var_init} 
 
