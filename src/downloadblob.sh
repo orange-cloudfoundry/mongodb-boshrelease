07fa6e5e42328f419e47541927b42f6081693399
Jérémy RAVERDY <jeremy.raverdy@orange.com>
2019-02-08 14:30:06 +0100

    packages was needed by compilation release not by the main release

--- src/downloadblob.sh
+++ src/downloadblob.sh
@@ -1,31 +1,3 @@
-curl -o cmake-3.8.2.tar.gz -L https://cmake.org/files/v3.8/cmake-3.8.2.tar.gz
-curl -o bzip2-1.0.6.tar.gz -L http://www.bzip.org/1.0.6/bzip2-1.0.6.tar.gz
-curl -o lz4-1.7.5.tar.gz -L https://github.com/lz4/lz4/archive/v1.7.5.tar.gz
-curl -o snappy-1.1.4.tar.gz -L https://github.com/google/snappy/archive/1.1.5.tar.gz
-curl -o zlib-1.2.11.tar.gz -L https://zlib.net/zlib-1.2.11.tar.gz
-curl -o zstd-1.3.0.tar.gz -L https://github.com/facebook/zstd/archive/v1.3.0.tar.gz
-curl -o gcc-5.4.0.tar.gz -L https://ftp.gnu.org/gnu/gcc/gcc-5.4.0/gcc-5.4.0.tar.gz
-curl -o gmp-6.1.2.tar.bz2 -L https://ftp.gnu.org/gnu/gmp/gmp-6.1.2.tar.bz2
-curl -o libpcap-1.8.1.tar.gz -L http://www.tcpdump.org/release/libpcap-1.8.1.tar.gz
-curl -o m4-1.4.18.tar.bz2 -L https://ftp.gnu.org/gnu/m4/m4-1.4.18.tar.bz2
-curl -o mpc-1.0.3.tar.gz -L https://ftp.gnu.org/gnu/mpc/mpc-1.0.3.tar.gz
-curl -o mpfr-3.1.5.tar.bz2 -L https://ftp.gnu.org/gnu/mpfr/mpfr-3.1.5.tar.bz2
-curl -o isl-0.18.tar.gz -L http://isl.gforge.inria.fr/isl-0.18.tar.gz
-curl -o go1.4-bootstrap-20170531.tar.gz -L https://storage.googleapis.com/golang/go1.4-bootstrap-20170531.tar.gz
-curl -o go1.8.3.tar.gz -L https://github.com/golang/go/archive/go1.8.3.tar.gz
-curl -o mongodb-src-r3.4.6.tar.gz -L 'https://fastdl.mongodb.org/src/mongodb-src-r3.4.6.tar.gz?_ga=2.231154774.896883508.1500285684-1291095056.1496324799'
-curl -o mongo-rocks-3.4.6.tar.gz -L https://github.com/mongodb-partners/mongo-rocks/archive/r3.4.6.tar.gz
-curl -o mongo-tools-3.4.6.tar.gz -L https://github.com/mongodb/mongo-tools/archive/r3.4.6.tar.gz
-curl -o rocksdb-5.5.5.tar.gz -L https://github.com/facebook/rocksdb/archive/v5.5.5.tar.gz
-curl -o Python-2.7.13.tgz -L https://www.python.org/ftp/python/2.7.13/Python-2.7.13.tgz
-curl -o scons-2.5.1.tar.gz -L https://ftp.osuosl.org/pub/blfs/conglomeration/scons/scons-2.5.1.tar.gz
-curl -o openssl-1.0.2l.tar.gz -L https://www.openssl.org/source/openssl-1.0.2l.tar.gz
-curl -o lzip-1.19.tar.gz -L http://download.savannah.gnu.org/releases/lzip/lzip-1.19.tar.gz
-curl -o ed-1.14.2.tar.lz -L https://ftp.gnu.org/gnu/ed/ed-1.14.2.tar.lz
-curl -o texinfo-6.4.tar.gz -L https://ftp.gnu.org/gnu/texinfo/texinfo-6.4.tar.gz
-curl -o bc-1.07.1.tar.gz -L https://ftp.gnu.org/gnu/bc/bc-1.07.1.tar.gz
-curl -o binutils-2.28.tar.gz -L https://ftp.gnu.org/gnu/binutils/binutils-2.28.tar.gz
-curl -o coreutils-8.27.tar.xz -L https://ftp.gnu.org/gnu/coreutils/coreutils-8.27.tar.xz
 wget https://s3-us-west-1.amazonaws.com/cf-cli-releases/releases/v6.30.0/cf-cli_6.30.0_linux_x86-64.tgz
 git clone https://github.com/JCL38-ORANGE/cf-mongodb-example-app.git
 
