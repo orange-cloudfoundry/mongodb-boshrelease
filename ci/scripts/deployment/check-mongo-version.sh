cde9d190b7978d6b72643f887f0662d0b2b044aa
Jérémy RAVERDY <jeremy.raverdy@orange.com>
2019-04-02 12:11:05 +0200

    Open a tunnel if ci is deployed through a bosh lite

--- ci/scripts/deployment/check-mongo-version.sh
+++ ci/scripts/deployment/check-mongo-version.sh
@@ -6,6 +6,18 @@ export BOSH_CONFIG=$PWD/bosh-director-config/bosh_config.yml
 
 ROOT_FOLDER=${PWD}
 
+# if mongodb deployment is realized on a bosh lite server, we need to open a tunnel to it in order to 
+# reach the containers ips
+
+exit 1
+
+if [ "${USE_BOSH_LITE}" == "true" ]
+then
+	echo "${JUMPBOX_KEY}" > /tmp/jumbox.key
+	chmod 600 /tmp/jumbox.key
+	sshuttle -r jumpbox@${BOSH_IP} -e 'ssh -i /tmp/jumbox.key -o ServerAliveInterval=60 -o ExitOnForwardFailure=yes'  --dns 10.244.0.0/16 --pidfile=/var/run/sshuttle-bosh-lite.pid
+fi	
+
 cat ${ROOT_FOLDER}/deployment-specs/keyval.properties \
   | grep -v -E "^UPDATED|^UUID" \
   > ${ROOT_FOLDER}/deployment-specs/sourced.properties
