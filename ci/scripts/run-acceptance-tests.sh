31b1cfe0519014541c99cab4984d0eb0359f5668
Jérémy RAVERDY <jeremy.raverdy@orange.com>
2019-04-05 11:08:56 +0200

    Updating errand name

--- ci/scripts/run-acceptance-tests.sh
+++ ci/scripts/run-acceptance-tests.sh
@@ -6,4 +6,4 @@ export BOSH_CONFIG=$PWD/bosh-director-config/bosh_config.yml
 
 ROOT_FOLDER=${PWD}
 
-bosh -e ${ALIAS} -d ${DEPLOYMENT_NAME} run-errand acceptance-tests
+bosh -e ${ALIAS} -d ${DEPLOYMENT_NAME} run-errand mongodb-acceptance-tests
