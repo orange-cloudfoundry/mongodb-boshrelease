747920694ebf29815607de683fac30013bcb74b1
Jérémy RAVERDY <jeremy.raverdy@orange.com>
2019-04-05 15:52:34 +0200

    Changing cf-cli docker image

--- ci/scripts/create-service-broker.sh
+++ ci/scripts/create-service-broker.sh
@@ -1,6 +1,21 @@
-#!/usr/bin/env bash 
+#!/usr/bin/env sh 
 
 set -ex
+
+
+#unset http_proxy https_proxy no_proxy
+
+export http_proxy=http://minio.dbsp.dw:3128
+export https_proxy=http://minio.dbsp.dw:3128
+
+if [ "${USE_BOSH_LITE}" == "true" ]
+then
+	echo "${JUMPBOX_KEY}" > /tmp/jumpbox.key
+	chmod 600 /tmp/jumpbox.key
+	sshuttle -D -r jumpbox@${BOSH_IP} -e 'ssh -i /tmp/jumpbox.key -o ServerAliveInterval=60 -o ExitOnForwardFailure=yes -o StrictHostKeyChecking=no' 10.244.0.0/16 --dns --pidfile=/var/run/sshuttle-bosh-lite.pid
+	#sleep 5
+fi
+
 
 if [ "${SKIP_SSL}" == "true" ]
 then
@@ -14,3 +29,4 @@ cf create-service-broker mongodb \
             mongodb-broker $(cat broker-password/password.txt) \
             http://mongodb-broker-mongodb-ci-deployment.cf.dbsp.dw --space-scoped
 
+[[ -f /var/run/sshuttle-bosh-lite.pid ]] && kill $(cat /var/run/sshuttle-bosh-lite.pid)            
