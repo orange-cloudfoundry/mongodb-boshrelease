261ebfb7cc04a005f9f62b05a38b443b0545f303
Jérémy RAVERDY <jeremy.raverdy@orange.com>
2019-04-09 17:23:01 +0200

    Removing broker opsfile

--- ci/scripts/create-service-broker.sh
+++ ci/scripts/create-service-broker.sh
@@ -5,14 +5,11 @@ set -ex
 
 #unset http_proxy https_proxy no_proxy
 
-export http_proxy=http://minio.dbsp.dw:3128
-export https_proxy=http://minio.dbsp.dw:3128
-
 if [ "${USE_BOSH_LITE}" == "true" ]
 then
 	echo "${JUMPBOX_KEY}" > /tmp/jumpbox.key
 	chmod 600 /tmp/jumpbox.key
-	sshuttle -D -r jumpbox@${BOSH_IP} -e 'ssh -i /tmp/jumpbox.key -o ServerAliveInterval=60 -o ExitOnForwardFailure=yes -o StrictHostKeyChecking=no' 10.244.0.0/16 --dns --pidfile=/var/run/sshuttle-bosh-lite.pid
+	sshuttle -D -r jumpbox@${BOSH_IP} -e 'ssh -i /tmp/jumpbox.key -o ServerAliveInterval=60 -o ExitOnForwardFailure=yes -o StrictHostKeyChecking=no' 0/0 --dns --pidfile=/var/run/sshuttle-bosh-lite.pid
 	#sleep 5
 fi
 
