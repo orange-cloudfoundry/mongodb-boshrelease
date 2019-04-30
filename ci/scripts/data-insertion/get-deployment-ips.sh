91ffcab0f2efe1ea3b5750af694e659f5577dda0
Jérémy RAVERDY <jeremy.raverdy@orange.com>
2019-04-30 15:50:17 +0200

    In shard mode, only mongos ips are needed

--- ci/scripts/data-insertion/get-deployment-ips.sh
+++ ci/scripts/data-insertion/get-deployment-ips.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+apt-get install -qqy jq
+
 export ROOT_FOLDER=${PWD}
 
 export BOSH_CONFIG=$PWD/bosh-director-config/bosh_config.yml
@@ -10,9 +12,20 @@ pushd output || exit 666
 
 [ -d ${ROOT_FOLDER}/deployment-specs ] && cp -rp ${ROOT_FOLDER}/deployment-specs/* .
 
-bosh -e ${ALIAS} -d ${DEPLOYMENT_NAME} vms --column Ips | sed -e 's/[[:space:]]*$/,/g' \
-			|tr -d "\n" \
-			|sed -e 's/,$//' -e 's/^/ips=\"/' -e 's/$/\"/' \
-			>> keyval.properties
+# In a shard configuration, only indicate the mongos ips
+bosh -e ${ALIAS} -d ${DEPLOYMENT_NAME} instances --ps --json \
+	|jq -r '.Tables[].Rows[]|select((.process|contains("routing_service")) or (.process|contains("mongodb_server")))|.instance' \
+	|xargs -i sh -c 'bosh -e '${ALIAS}' -d '${DEPLOYMENT_NAME}'  vms --json|jq -r ".Tables[].Rows[]|select(.instance|contains(\"$1\"))|.ips"' {} {} \
+	|sort -u \
+	|tr -s '\n' ','\
+	|sed -e 's/,$//g' -e 's/^/ips=\"/' -e 's/$/\"/' \
+	>> keyval.properties
+
+echo $?	
+
+#bosh -e ${ALIAS} -d ${DEPLOYMENT_NAME} vms --column Ips | sed -e 's/[[:space:]]*$/,/g' \
+#			|tr -d "\n" \
+#			|sed -e 's/,$//' -e 's/^/ips=\"/' -e 's/$/\"/' \
+#			>> keyval.properties
 
 popd
